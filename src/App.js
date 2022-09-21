19c856c3bd046dd7f0d9473313e158d17312fb02
Христюк Ксения Алексеевна <@itstep.loc>
2022-09-21 17:58:31 +0600

    modal done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Header from './components/header/header';
 import Programs from './components/programs/programs';
 
 import Login from './Login';
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes, useState} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 import Main from './components/main/main';
 
 
+
 const App =()=>{
-  
+  // const [modalActive, setModalActive] = useState(true)
   return( 
     <div>
       <div className='two'>
@@ -27,6 +28,15 @@ const App =()=>{
     <Link to="/donation">donation </Link>
   </li>
 </ul>
+{/* <button className='open-btn' onClick={() => setModalActive(true)}>открыть окно</button> */}
+{/* <Modal active={modalActive} setActive={setModalActive}>
+  <form action=''>
+    <input type="text"> </input>
+    <input type="text"> </input>
+    <input type="text"> </input>
+    <button> SEND</button>
+  </form>
+</Modal> */}
 </div>
 
       <Routes>
@@ -35,6 +45,7 @@ const App =()=>{
       <Route path="/login" element={<Login/>}/>
       <Route path="/donation" element={<Donation/>}/>
       </Routes>
+      
   </div>
   )
 }
--- src/components/programs/programs.js
+++ src/components/programs/programs.js
@@ -1,40 +1,102 @@
-import './programs.css';
-import dogs from '../images/friends.jpg';
-import trash from '../images/мусор.png';
-import tree from '../images/tree.webp';
+import "./programs.css";
+import dogs from "../images/friends.jpg";
+import trash from "../images/мусор.png";
+import tree from "../images/tree.webp";
 
+import React from "react";
 
-const Programs =()=> {
+const Programs = () => {
+  const [open, setOpen] = React.useState(false);
 
   return (
     <div className="body">
-    <h1>Able programs in Kazakhstan:</h1>
-<div className="wrapper">
-<div className="media">
-<div className="layer">
-<h1>очистка города</h1>
-    <button className="custom-btn btn-16">записаться</button>
-  </div>
-<img src={trash}/>
-</div>
- <div className="media">
-<div className="layer">
-    <h1>помощь приюту</h1>
-    <button className="custom-btn btn-16">записаться</button>
-  </div>
-<img src={dogs} alt="" />
-</div>
-   <div className="media">
-<div className='layer'>
-    <h1>озеленение участка</h1>
-    <button className="custom-btn btn-16">записаться</button>
-    
-  </div>
-<img src={tree} alt="" />
-</div>
-</div>
-</div>
-  )
-  }
-  
-  export default Programs
+      <h1>Able programs in Kazakhstan:</h1>
+      <div className="wrapper">
+        <div className="media">
+          <div className="layer">
+            <h1>очистка города</h1>
+            <div className="App">
+              <button onClick={() => setOpen(true)} className="open-modal-btn">
+                {" "}
+                записаться
+              </button>
+           
+            </div>
+            {open && (
+                <div className="overlay animated">
+                <div  className="modal">
+                  <div className="column">
+                  <h1 onClick={() => setOpen(false)} className={` overlay animated ${open ? 'show' : ''} `}>оставьте данные для обратной связи</h1>
+                  <div>
+                  <input type="text" placeholder="your surname" ></input>
+                    <input type="text" placeholder="your name"></input>
+                    <input type="text" placeholder="your email"></input>
+                    <button className="sign"> записаться </button>
+                    </div>
+                    </div>
+                </div>
+              </div>
+              )}
+          </div>
+          <img src={trash} />
+        </div>
+        <div className="media">
+          <div className="layer">
+            <h1>помощь приюту</h1>
+            <div className="App">
+              <button onClick={() => setOpen(true)} className="open-modal-btn">
+                {" "}
+                записаться
+              </button>
+            </div>
+            {open && (
+              <div className="overlay animated">
+                <div  className="modal">
+                  <div className="column">
+                  <h1 onClick={() => setOpen(false)}>оставьте данные для обратной связи</h1>
+                  <div>
+                  <input type="text" placeholder="your surname" ></input>
+                    <input type="text" placeholder="your name"></input>
+                    <input type="text" placeholder="your email"></input>
+                    <button className="sign"> записаться </button>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            )}
+          </div>
+          <img src={dogs} alt="" />
+        </div>
+        <div className="media">
+          <div className="layer">
+            <h1>озеленение участка</h1>
+            <div className="App">
+              <button onClick={() => setOpen(true)} className="open-modal-btn">
+                {" "}
+                записаться
+              </button>
+            </div>
+            {open && (
+              <div className="overlay animated">
+              <div  className="modal">
+                <div className="column">
+                <h1 onClick={() => setOpen(false)}>оставьте данные для обратной связи</h1>
+                <div>
+                <input type="text" placeholder="your surname" ></input>
+                  <input type="text" placeholder="your name"></input>
+                  <input type="text" placeholder="your email"></input>
+                  <button className="sign"> записаться </button>
+                  </div>
+                  </div>
+              </div>
+            </div>
+            )}
+          </div>
+          <img src={tree} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Programs;
